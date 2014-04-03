68b115225ec2e41a9c7a4e4a7912353b86eee548
Nicky Sandoz <Nicky@nicky-sandozs-imac-2.local>
2014-04-02 19:56:47 -0700

    Initial Commit

---
+++ Variables/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Variables
+//
+//  Created by Nicky Sandoz on 4/2/14.
+//  Copyright (c) 2014 Nicky Sandoz. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
